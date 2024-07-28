c38d26d16883088ec612795e9782ce25be927dcb
yasyasench <yasyasench@gmail.com>
2024-07-28 21:39:15 +0200

    5&6 tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+btn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  span.textContent = randomColor
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("button[data-create]");
+const desrtoyBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let size = 30;
+  let htmlString = "";
+
+  for (let i = 0; i < amount; i++){
+    const color = getRandomHexColor();
+    htmlString += `<div style="background-color: ${color}; width : ${size}px; heigth: ${size}px;"></div>`;
+    size += 10;
+  }
+  boxes.innerHTML = htmlString; 
+}
+
+createBtn.addEventListener("click", () => {
+  const val = parseInt(input.value);
+  const atrMin = parseInt(input.getAttribute("min"));
+  const atrMax = parseInt(input.getAttribute("max"));
+
+  if (val >= atrMin && val <= atrMax) {
+    createBoxes(input.value);
+    input.value = "";
+  }
+});
+
+function desrtoyBoxes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+desrtoyBtn.addEventListener("click", () => {
+  desrtoyBoxes(boxes);
+});
